b67efffb388e0424a94077d48deb4b0921b8ded1
Henry Boles <henryjboles@gmail.com>
2022-03-03 20:20:38 -0800

    a

--- src/main.js
+++ src/main.js
@@ -22,9 +22,48 @@ function createTray () {
       }
     },
   ])
+  tray.on('click', function(e){
+    if (mainWindow) {
+      mainWindow = null;
+    } else {
+      createWindow();
+    }
+  });
 
   tray.setContextMenu(contextMenu)
+  tray.setIgnoreDoubleClickEvents(true)
 }
+
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    } else {
+      createWindow();
+    }
+  })
+
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+  })
+}
+
 
 function createWindow () {
   if (!tray) { // if tray hasn't been created already.
@@ -53,19 +92,6 @@ function createWindow () {
     mainWindow = null
   })
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
